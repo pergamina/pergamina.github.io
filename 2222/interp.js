556a865fea5dea9906eda0bcdaf93d78c2cd8ab3
pergamina <75643337+pergamina@users.noreply.github.com>
2020-12-26 09:15:21 -0300

    Add files via upload

--- 2222/interp.js
+++ 2222/interp.js
@@ -75,7 +75,7 @@ let SYMBOLS = {
 function evalCmd(full, cmd, elem) {
   clearElem(elem);
   if (full.length === cmd.length) return;
-  let newDiv = DIV('innercanvas', [buildElem(cmd, cmd.length, 0)]);
+  let newDiv = DIV('innercanvas', [buildElem(full, cmd, cmd.length, 0)]);
   elem.appendChild(newDiv);
 }
 
@@ -87,72 +87,44 @@ function isVowel(x) {
       || x === 'u';
 }
 
-let MAX_ROT = 20;
-
-function buildElem(cmd, i, depth) {
-  if (i === 0 || depth > 40) {
+function buildElem(full, cmd, i, depth) {
+  if (i === 0 || depth > 2 + 3 * (full.length % 5)) {
     let box = DIV('box', []);
-    box.style.border += ' solid white 2px';
+    //box.style.border += ' solid white 2px';
     return box;
   }
 
   let l = (cmd.length % 2 === 0) ? cmd[i - 1] : cmd[cmd.length - i - 1];
   let p = (l in SYMBOLS) ? SYMBOLS[l] : 0.5;
-  let rad = Math.floor(p * 50);
-  if (Math.cos(p * Math.PI) < 0) {
-    let box = DIV('box', []);
-    box.style.border = 'solid white 2px';
-    if (l === 'a') {
-      box.style.transform += ' rotate(1deg)';
-    } else if (l === 'e') {
-      box.style.transform += ' rotate(-1deg)';
-    } else if (l === 'i') {
-      box.style.transform += ' rotate(2deg)';
-    } else if (l === 'o') {
-      box.style.transform += ' rotate(-2deg)';
-    }
-
-    if (l === 'b' || l === 'c' || l === 'd' || l === 'f' || l === 'g') {
-      box.style.padding = '2%';
-    } else if (l === 'h' || l === 'j' || l === 'k' || l === 'l' || l === 'm') {
-      box.style.padding = '5%';
-    } else {
-      box.style.padding = '10%';
-    }
-
-    if (l === 'b' || l === 'f' || l === 'j' || l === 'm' || l === 'p') {
-      box.style.opacity = '0.75';
-    } else if (l === 'c' || l === 'g' || l === 'k' || l === 'n' || l === 'r') {
-      box.style.opacity = '0.7';
-    } else {
-      box.style.opacity = '0.7';
-    }
-    box.appendChild(buildElem(cmd, i - 1, depth + 1));
-    box.style.borderRadius = rad.toString() + '%';
-    return box;
-  } else {
-    let box = DIV('box', []);
-    let bsize = Math.floor(1 + p * 7);
-    box.style.border = 'solid white ' + bsize + 'px';
-
-    // Rotation
-    let deg = Math.floor(p * MAX_ROT - MAX_ROT / 2);
-    if (i % 2 === 0) {
-      deg = -deg;
-    }
-    box.style.transform += ' rotate(' + deg.toString() + 'deg)';
-
-    // Opacity
-    box.style.opacity = '0.9';
-    let size = 80 + Math.floor(Math.sin(p) * 20);
-    box.style.height = size.toString() + '%';
-    box.style.width = size.toString() + '%';
-    box.style.padding = Math.floor(p * 10).toString() + '%';
-
-    // Rounded
-    box.style.borderRadius = rad.toString() + '%';
-
-    box.appendChild(buildElem(cmd, i - 1, depth + 1));
-    return box;
+  let rad = (full.length % 3) * Math.floor(p * 20);
+  let box = DIV('box', []);
+  box.style.border = 'solid white 2px';
+  if (l === 'a') {
+    box.style.transform += ' rotate(1deg)';
+  } else if (l === 'e') {
+    box.style.transform += ' rotate(-1deg)';
+  } else if (l === 'i') {
+    box.style.transform += ' rotate(2deg)';
+  } else if (l === 'o') {
+    box.style.transform += ' rotate(-2deg)';
   }
+
+  if (l === 'b' || l === 'c' || l === 'd' || l === 'f' || l === 'g') {
+    box.style.padding = '2%';
+  } else if (l === 'h' || l === 'j' || l === 'k' || l === 'l' || l === 'm') {
+    box.style.padding = '5%';
+  } else {
+    box.style.padding = '10%';
+  }
+
+  if (l === 'b' || l === 'f' || l === 'j' || l === 'm' || l === 'p') {
+    box.style.opacity = '0.75';
+  } else if (l === 'c' || l === 'g' || l === 'k' || l === 'n' || l === 'r') {
+    box.style.opacity = '0.7';
+  } else {
+    box.style.opacity = '0.7';
+  }
+  box.appendChild(buildElem(full, cmd, i - 1, depth + 1));
+  box.style.borderRadius = rad.toString() + '%';
+  return box;
 }
